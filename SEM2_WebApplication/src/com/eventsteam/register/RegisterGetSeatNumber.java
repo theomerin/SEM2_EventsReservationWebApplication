348db18b523120e708cf7b904825d330f0840340
theomerin <theo.merin@gmail.com>
2015-04-18 22:42:30 +0800

    CSS is in!

--- SEM2_WebApplication/src/com/eventsteam/register/RegisterGetSeatNumber.java
+++ SEM2_WebApplication/src/com/eventsteam/register/RegisterGetSeatNumber.java
@@ -3,6 +3,7 @@ package com.eventsteam.register;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -49,7 +50,7 @@ public class RegisterGetSeatNumber extends HttpServlet {
 		}
 		response.setContentType("text/html");  
 		PrintWriter out = response.getWriter();
-		out.print(seatNumber + " " + seatType + " " +seatPrice);
+		RequestDispatcher rd=request.getRequestDispatcher("/reserve-now2.html");  
+        rd.include(request, response);
 	}
-
 }
